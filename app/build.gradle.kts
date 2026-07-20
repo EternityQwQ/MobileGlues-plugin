51503fe01af38f57f9b7778019b2e20cd6435a9b
Luna-QwQ <141827326+Luna-QwQ@users.noreply.github.com>
2026-07-20 11:22:04 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fcl.plugin.mobileglues.eternity"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1030
-        versionName = "1.0.3"
+        versionCode = 1040
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
